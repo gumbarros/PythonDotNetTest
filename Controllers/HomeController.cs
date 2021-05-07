88d9c8d845ffec3bc44b771bf3a02cda8b5e1dc1
gumbarros <gustavomauriciodebarros@gmail.com>
2021-05-06 22:44:33 -0300

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PythonDotNetTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PythonDotNetTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string code)
+        {
+
+            var result = RunPython(code);
+
+            ViewBag.Result = result.Result;
+            ViewBag.Error = result.Error;
+            ViewBag.Code = code;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public PythonResult RunPython(string code)
+        {
+            ProcessStartInfo start = new()
+            {
+                FileName = "python",
+                Arguments = string.Format("-c \"{0}\"", code),
+                UseShellExecute = false,
+                CreateNoWindow = false,
+                Verb = "runas",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using Process process = Process.Start(start);
+            using StreamReader reader = process.StandardOutput;
+
+            var result = new PythonResult
+            {
+                Error = process.StandardError.ReadToEnd(), 
+                Result = reader.ReadToEnd()
+            };
+
+            return result;
+        }
+    }
+
+}
---
+++ Models/PythonResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PythonDotNetTest.Models
+{
+    public class PythonResult
+    {
+        public string Result;
+        public string Error;
+    }
+}
